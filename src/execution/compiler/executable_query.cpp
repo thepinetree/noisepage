96f38c50a346c0e717c2d09574844e7c0877eaf6
Tanuj Nayak <tanujnay112@live.com>
2020-09-04 15:39:13 -0700

    timer

--- src/execution/compiler/executable_query.cpp
+++ src/execution/compiler/executable_query.cpp
@@ -152,13 +152,19 @@ void ExecutableQuery::Run(common::ManagedPointer<exec::ExecutionContext> exec_ct
   auto query_state = std::make_unique<byte[]>(query_state_size_);
   *reinterpret_cast<exec::ExecutionContext **>(query_state.get()) = exec_ctx.Get();
 
-  exec_ctx->SetExecutionMode(static_cast<uint8_t>(mode));
-  exec_ctx->SetPipelineOperatingUnits(GetPipelineOperatingUnits());
+  double elapsed_ms;
+  {
+    util::ScopedTimer timer(&elapsed_ms);
+    exec_ctx->SetExecutionMode(static_cast<uint8_t>(mode));
+    exec_ctx->SetPipelineOperatingUnits(GetPipelineOperatingUnits());
 
-  // Now run through fragments.
-  for (const auto &fragment : fragments_) {
-    fragment->Run(query_state.get(), mode);
+    // Now run through fragments.
+
+    for (const auto &fragment : fragments_) {
+      fragment->Run(query_state.get(), mode);
+    }
   }
+  std::cout << "Query took " << elapsed_ms << " ms to run\n";
 }
 
 }  // namespace terrier::execution::compiler
